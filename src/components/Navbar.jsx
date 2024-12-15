6782e105c750c9092a73d42ad91ab64f2bdeec9f
Martin Odongo <martinwere14@gmail.com>
2024-12-15 03:42:31 +0300

    feat: made the navbar and styled it using tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="absolute w-full px-16 py-2 flex flex-row items-center  justify-between text-[#A8AEBF]  font-Poppins font-bold">
+    <nav className="absolute w-full px-16 py-2 flex flex-row items-center  justify-between text-[#A8AEBF]  bg-[#121829CC] font-Poppins font-bold">
       <div className="h-12 w-12 ">
         <img src="/images/Logo.svg" alt="logo" />
       </div>
